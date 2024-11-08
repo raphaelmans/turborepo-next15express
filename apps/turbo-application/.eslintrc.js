630721710fc0891b2c9aadb4c447932fe7dd0da9
Raphael Mansueto <raphaelmansueto@gmail.com>
2024-11-09 05:14:48 +0800

    feat: added turbo-application app

---
+++ apps/turbo-application/.eslintrc.js
@@ -0,0 +1,21 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:prettier/recommended",
+    "@repo/eslint-config/next.js",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
